5cf5feda56e99fb58a18bf97a368241754b24863
Rachel Lense <rachel.lense@ampf.com>
2019-03-15 15:48:06 -0500

    update App to extend React.Component

--- src/App.js
+++ src/App.js
@@ -7,25 +7,27 @@ const Pet = (props) => {
   ])
 }
 
-const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'dog',
-      breed: 'Havanese'
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'bird',
-      breed: 'Cockatiel'
-    }),
-    React.createElement(Pet, {
-      name: 'Doink',
-      animal: 'cat',
-      breed: 'Mixed'
-    })
-  ])
+class App extends React.Component {
+  render() {
+    return React.createElement('div', {}, [
+      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement(Pet, {
+        name: 'Luna',
+        animal: 'dog',
+        breed: 'Havanese'
+      }),
+      React.createElement(Pet, {
+        name: 'Pepper',
+        animal: 'bird',
+        breed: 'Cockatiel'
+      }),
+      React.createElement(Pet, {
+        name: 'Doink',
+        animal: 'cat',
+        breed: 'Mixed'
+      })
+    ])
+  }
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))

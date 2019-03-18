b80674ffd96a91691d469d777dd252a501a7ae0f
Rachel Lense <rachel.lense@ampf.com>
2019-03-18 13:49:59 -0500

    initialize project add gitignore install prettier apply prettier to App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed)
-
-  ])
-}
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
 
 class App extends React.Component {
   render() {
-    return React.createElement('div', {}, [
-      React.createElement('h1', {}, 'Adopt Me!'),
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt Me!"),
       React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'dog',
-        breed: 'Havanese'
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
       }),
       React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'bird',
-        breed: 'Cockatiel'
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
       }),
       React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'cat',
-        breed: 'Mixed'
+        name: "Doink",
+        animal: "cat",
+        breed: "Mixed"
       })
-    ])
+    ]);
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
